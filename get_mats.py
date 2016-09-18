7c2826d4de57029f3ab5b494d26470da59bfbd3b
prevosis <aadilzbhatti@gmail.com>
2016-09-17 21:15:51 -0400

    Adding mats

--- get_mats.py
+++ get_mats.py
@@ -2,9 +2,9 @@ import os
 import librosa
 import numpy as np
 
-for root, _, files in os.walk('./test_data'):
+for root, _, files in os.walk('./test_data/raw'):
     for name in files:
         datapath = os.path.join(root, name)
         y, sr = librosa.load(datapath)
         S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
-        np.savetxt(name + ".txt", S)
+        np.savetxt(os.path.join('..', name + ".txt"), S)
